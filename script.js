1bb2a295dde048f4121bfb86f3681115e5aab11a
mvargaslo-create <mvargaslo@unal.edu.co>
2026-04-29 19:22:37 -0500

    script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const form = document.getElementById("formulario");
 
-    form.addEventListener("submit", async function(e){
-        e.preventDefault();
+    form.addEventListener("submit", async function(e){         // Captura el evento submit del formulario
+                e.preventDefault();
         
         let edad = parseInt(document.getElementById("edad").value);
         let genero = document.getElementById("genero").value;
         let departamento = document.getElementById("departamento").value;
         let cgpa = parseFloat(document.getElementById("cgpa").value);
-        let sueno = parseFloat(document.getElementById("sueno").value);
-        let estudio = parseFloat(document.getElementById("estudio").value);
-        let redes = parseFloat(document.getElementById("redes").value);
-        let actividad = parseFloat(document.getElementById("actividad").value);
-        let estres = parseFloat(document.getElementById("estres").value);
+        let horasSueno = parseFloat(document.getElementById("sueno").value);
+        let horasEstudio = parseFloat(document.getElementById("estudio").value);
+        let horasRedes = parseFloat(document.getElementById("redes").value);
+        let actividadFisica = parseFloat(document.getElementById("actividad").value);
+        let nivelEstres = parseFloat(document.getElementById("estres").value);
 
         try {
             let response = await fetch("http://127.0.0.1:5000/predict", {
@@ -26,11 +26,11 @@ document.addEventListener("DOMContentLoaded", function () {
                     genero,
                     departamento,
                     cgpa,
-                    sueno,
-                    estudio,
-                    redes,
-                    actividad,
-                    estres
+                    horasSueno,
+                    horasEstudio,
+                    horasRedes,
+                    actividadFisica,
+                    nivelEstres
                 })
             });
 
